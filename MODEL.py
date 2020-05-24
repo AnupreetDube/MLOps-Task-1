fe1db7df949e09dd304709ae5cd5a4e4c28f3d6d
Anupreet <64468290+AnupreetDube@users.noreply.github.com>
2020-05-24 19:43:04 +0530

    Update MODEL.py

--- MODEL.py
+++ MODEL.py
@@ -1,12 +1,14 @@
-#IMPORTING MNIST DATASET
-from keras.datasets import mnist
+#IMPORTING REQUIRED MODULES/FUNCTIONS
 from keras.utils import to_categorical as tc
 from keras.models import Sequential as seq
 from keras.layers import Dense, Conv2D, Flatten
+#IMPORTING MNIST DATASET
+from keras.datasets import mnist
 
 #DATASET SPLITTING
 (xtrain,ytrain) , (xtest,ytest) = mnist.load_data()
 
+#PRE-PROCESSING
 #RESHAPING
 xtrain=xtrain.reshape(60000,28,28,1)
 xtest=xtest.reshape(10000,28,28,1)
@@ -25,20 +27,23 @@ FilterSize=4
 for NbrOfConvLayers in range(NbrOfConvLayers):
 	model.add(Conv2D(filters=FilterSize, kernel_size=3, activation="relu"))
 	FilterSize*=2
-
+	
 model.add(Flatten())
 model.add(Dense(10, activation="softmax"))
+#COMPILING ALL THE LAYERS TOGETHER
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+#FITTING THE TRAINING DATASET INTO THE MODEL
 model.fit(xtrain, ytrain,epochs=1)
 
-#PREDICTION
-pred= (model.evaluate(xtest,ytest))*100
-print("Accuracy : ", pred[1])
+#PREDICTION WITH TESTING DATA
+accuracy= (model.evaluate(xtest,ytest))*100
+print("Accuracy : ", accuracy[1])
 
 #PUTTING THE ACCURACY DATA INTO A FILE 
 try:
 	f=open("/accuracy/acc.txt","w")
-	f.write(str(int(pred[1])))
+	f.write(str(int(accuracy[1])))
 except:
 	print(end="")
 finally:
